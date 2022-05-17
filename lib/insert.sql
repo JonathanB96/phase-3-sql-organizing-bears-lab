91811850190e120fcd526ae65c228fdd8e424da5
jonathan bola <bolajonathan@yahoo.com>
2022-05-17 16:42:58 -0500

    commit tree 3a795e5cbf65b93cd4d60f206cce19a5fbac22d1

---
+++ lib/insert.sql
@@ -0,0 +1,25 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Mr. Chocolate", 45, "M", "frfae","ewfe", 1
+
+);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Rowdy", 15, "F", "ewjej","ewfe", 0
+
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Tabitha", 41, "M", "ewfmerng","ewfe", 1
+
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Sergeant Brown", 17, "M", "3jedhf","ewfe", 0
+
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Melissa", 19, "F", "weferf","ewfe", 0
+
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Grinch", 4545, "M", "green","ewfe", 1
+
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Wendy", 14, "F", "wdeas","ewfe", 1
+
+);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(NULL, 7, "F", "wfkwee","ewfe", 1
+
+);
